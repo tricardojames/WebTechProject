ac0f17522add4e3fab7b5dfa27f38c521bfb587a
tricardojames <89865971+tricardojames@users.noreply.github.com>
2022-12-06 19:32:15 -0800

    updates

--- script.js
+++ script.js
@@ -21,3 +21,25 @@ let scene = new ScrollMagic.Scene({
 .setTween(timeline)
 .setPin("section")
 .addTo(controller);
+
+/* scroll down*/
+
+const sections = document.querySelector("section");
+const navLi = document.querySelectorAll("nav .container ul li");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
+
